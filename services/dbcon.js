e0083d3de6ba89e2b381a4b62031163bc96e23a2
Patrick Glowacki <51968783+pglow11@users.noreply.github.com>
2021-08-16 20:34:38 -0500

    Update dbcon.js

--- services/dbcon.js
+++ services/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu',
-  user: 'cs361_glowackp',     // replace with your credientials
-  password: '4956',           // replace with your credientials
+  user: '[user_name]',     // replace with your credientials
+  password: '[password]',           // replace with your credientials
   database: 'cs361_glowackp'  // replace with your credientials
 });
 
